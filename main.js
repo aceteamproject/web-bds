0a55e9297d016a9d474dee37728ac727b892a14f
Hoàng <daoduyhoang1994@gmail.com>
2021-05-15 22:56:57 +0700

    fix header

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ $(".block").click(function () {
 });
 
 $(".fa-times").click(function () {
-  $(".main-menu-mobile").animate({ right: "-50%" });
+  $(".main-menu-mobile").animate({ right: "-100%" });
   $("body,html").css("overflow-y", "unset");
 });
+
+if ($(window).width() <= 768) {
+  $("#header .header-mb").removeClass("container");
+  $("#header .header-mb").addClass("container-fluid");
+}
