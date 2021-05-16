db36b2adf8b174fc63703c0819a0907f1771c9b8
Hoàng <daoduyhoang1994@gmail.com>
2021-05-16 11:36:11 +0700

    fix details

--- main.js
+++ main.js
@@ -12,3 +12,21 @@ if ($(window).width() <= 768) {
   $("#header .header-mb").removeClass("container");
   $("#header .header-mb").addClass("container-fluid");
 }
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() >= 50) {
+    // console.log("okokokok");
+    $(".sticky-menu").addClass("fixed-header");
+    $(".main-menu li a").addClass("color-sticky");
+    $(".scroll-ontop").addClass("show");
+  } else {
+    $(".sticky-menu").removeClass("fixed-header");
+    $(".main-menu li a").removeClass("color-sticky");
+    $(".scroll-ontop").removeClass("show");
+  }
+});
+
+// SCROLL ON TOP
+$(".scroll-ontop").on("click", function () {
+  $("html, body").animate({ scrollTop: 0 }, "300");
+});
